f83abe4ef3087b3580ec6039394410e0ab5e50d8
rondarmon <ron.darmon@gmail.com>
2023-06-10 12:54:46 +0300

    updated to full answers

--- hw1/hw1/answers.py
+++ hw1/hw1/answers.py
@@ -11,29 +11,45 @@ math (delimited with $$).
 part1_q1 = r"""
 **Your answer:**
 
-1) **False.**<br>
-In-sample error (which is also known as training error), is the error on the training set,<br>
-and it is not a good measure of the model's performance on unseen data.<br>
-The test set is used to evaluate the model's performance on unseen data.<br>
-Hence, the test set allows us to estimate the out of sample error and not the in-sample error.<br>
+1) **False**.
+In-sample error, also known as training error, measures the error of the model on the training set.
+It represents how well the model fits the training data.
+However, it does not provide an accurate estimate of the model's performance on unseen data. 
+The goal of machine learning is to create a model that can generalize well to new, unseen data. 
+To evaluate the model's performance on such data, we use a separate set called the test set.
+The test set allows us to estimate the out-of-sample error, which provides a more reliable measure of the model's 
+performance in real-world scenarios.
 
+2) **False**.
+Not all splits of the data into two disjoint subsets would constitute equally useful train-test splits.
+There are several considerations to keep in mind when creating a train-test split. for example:
+* Imbalanced dataset: If the dataset is imbalanced, meaning one class is significantly more prevalent than the others, 
+a random split may result in an unequal distribution of classes in the train and test sets.
+In such cases, it is important to use techniques like stratification to ensure that the class distribution is preserved in both subsets.
+* Shuffling: It is crucial to shuffle the data before splitting it into train and test sets. 
+Failure to do so may lead to ordered patterns in the data, causing the model to learn based on that order and perform poorly on new, unseen data.
+* Time series data: If the dataset contains time series data, it is essential to consider the temporal aspect. 
+The train set should consist of data from earlier time periods, while the test set should contain data from later time periods. 
+This ensures that the model is evaluated on its ability to generalize to future time points.
+* Size of train and test sets: Extremely small train or test sets may result in poor performance or unreliable evaluation. 
+Having an adequate amount of data in both subsets is important for the model to learn meaningful patterns and for the evaluation to be statistically robust.
+Taking these factors into account, it becomes clear that not all splits are equally useful, and careful consideration should be given to creating an appropriate train-test split.
 
-2) **False.**<br>
-The following are all examples of bad data splits:<br>
-Imbalanced dataset may require stratification.<br>
-Splitting without shuffling is a bad practice.<br>
-With time series data, the train set time is earlier then the test set time.<br>
-Also, very small train set or very small test set, may result in bad performance or bad out of sample evaluation.<br>
+3) **True**.
+The test set should not be used during cross-validation.
+Cross-validation is a technique used to assess the performance of a model by splitting the data into multiple subsets, or folds,
+and iteratively training and evaluating the model on different combinations of these folds. 
+The purpose of cross-validation is to obtain an unbiased estimate of the model's performance on unseen data.
+If the test set were used during cross-validation, it would result in data leakage. 
+Data leakage occurs when information from the test set is unintentionally used during model training or tuning, leading to overoptimistic performance estimates.
+To prevent this, the test set should be kept completely separate and used only once, after the model has been finalized, to evaluate its performance.
 
-3) **True.**<br>
-The test set should not be used for cross validation,<br>
-because it is used to evaluate the model's performance on unseen data.<br>
+4) **True.**
+After performing cross-validation, the validation-set performance of each fold can be used as a proxy for the model's generalization error.
+Cross-validation provides a robust estimate of the model's performance by evaluating it on multiple subsets of the data.
+By averaging the performance across different folds, we obtain a more reliable indication of how well the model is likely to generalize to new, unseen data.
 
-4) **True.**<br>
-The validation set can be used as a proxy for the model generalization error,<br>
-Because it is used to evaluate the model's performance on unseen data during the training process,<br>
-with different train, validation splits.<br>
-
+.
 """
 
 part1_q2 = r"""
@@ -54,26 +70,49 @@ This method leads to an overly optimistic evaluation of the model's performance.
 part2_q1 = r"""
 **Your answer:**
 
-Increasing the number of neighbors (k), can lead to better generalisation for unseen data up to a certain point.<br>
-However, if we increase k too much, we will start to see a decrease
-in performance (too many neighbors, underfitting).<br>
-On the other hand, if we decrease k too much, we will start to see a decrease in performance due to overfitting.<br>
+Increasing the number of neighbors (k) in K-nearest neighbors (KNN), has the potential to improve the model's generalization capability 
+for unseen data up to a certain threshold. 
+By considering more neighbors, the model can incorporate a broader range of information from the training data and make predictions based on a larger pool of similar instances. 
+However, there is a point where increasing k too much can have a detrimental effect on performance. 
+When the number of neighbors becomes excessively large, the model may start to suffer from underfitting, where it fails to capture the underlying patterns in the data and becomes overly simplistic. 
+This is because an excessive number of neighbors can reduce the influence of relevant training instances, leading to less discriminative decision boundaries and decreased accuracy. 
+
+<br> Conversely, decreasing k too much can lead to overfitting, where the model becomes overly sensitive to the training data and fails to generalize well to unseen examples.
+In this case, the model might become too specialized to the training set, capturing noise and irrelevant patterns that do not hold true in the broader context. 
+
+<br> Therefore, finding the optimal value for k is crucial in striking a balance between bias and variance, 
+and it typically involves experimentation and validation to determine the most suitable number of neighbors for a given dataset and problem.
+
+
+-
 
 """
 
 part2_q2 = r"""
 **Your answer:**
+Using k-fold cross-validation offers several advantages over the alternative approaches of training on the entire 
+train-set and selecting the best model based on either the train-set or test-set accuracy.
 
-1) Using k-fold cross validation is better than using the training score of a specific split,
-Evaluating the model based on the training score of a specific split may lead to overfitting.<br>
-Especially in the case of selecting k neighbors of 1 (100% accuracy).<br>
+1) When training on the entire train-set and selecting the best model based on the train-set accuracy, there is a risk of overfitting.
+This approach can inadvertently favor complex models that fit the training data extremely well but may not generalize effectively to unseen data. 
+By evaluating models solely on their performance on the training set, we may end up selecting a model that is overly specialized to the idiosyncrasies of the training data, 
+resulting in poor performance on new, unseen instances. In contrast, k-fold cross-validation helps mitigate this risk by providing a more robust estimate of the model's performance on unseen data.
 
-2) Using k-fold cross validation is better than using the test score of a specific split,
-both because it gives a better proxy for the unseen data and the model selection is not based on a specific split.<br>
-Furthermore, evaluating the model based on the test score of a specific split may lead to overfitting,
-as we select the hyper parameters based on a specific test score,
-which can lead to a very specific generalisation based on that split.<br>
-However, cross validation is computationally more expensive than using a single split.<br>
+2) Similarly, using the test-set accuracy for model selection can also lead to overfitting. 
+When we evaluate multiple models on the same test set and choose the one with the best performance, there is a danger of "data leakage". 
+The models may inadvertently learn patterns specific to that particular test set, which may not hold true for new, unseen data
+Consequently, the selected model may not perform as well when deployed in a real-world setting.
+In contrast, k-fold cross-validation provides a better proxy for unseen data because it evaluates the models on multiple partitions of the data,
+ensuring that the model's performance is assessed on a more diverse range of instances.
+
+Moreover, k-fold cross-validation reduces the dependence on a single split for model selection. 
+By partitioning the data into multiple folds and systematically rotating which fold is used as the validation set, 
+we obtain a more comprehensive evaluation of the models across different subsets of the data. 
+This reduces the influence of any peculiarities in a particular split, yielding a more reliable estimate of the model's performance.
+
+It is worth noting that k-fold cross-validation is computationally more expensive than using a single split.
+Since the model is trained and evaluated multiple times on different folds, it requires more computational resources. 
+However, the additional computational cost is justified by the improved reliability and generalization ability of the model.
 
 """
 
