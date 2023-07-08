b70d4299621ffbee49ee725e14e97806e4153f36
RoeiArpaly <roei.arpaly@gmail.com>
2023-07-08 12:45:49 +0300

    set hyperparameters

--- hw3/hw3/answers.py
+++ hw3/hw3/answers.py
@@ -22,7 +22,14 @@ def part1_rnn_hyperparams():
     )
     # TODO: Set the hyperparameters to train the model.
     # ====== YOUR CODE: ======
-    
+    hypers['batch_size'] = 64
+    hypers['seq_len'] = 128
+    hypers['h_dim'] = 256
+    hypers['n_layers'] = 3
+    hypers['dropout'] = 0.2
+    hypers['learn_rate'] = 0.001
+    hypers['lr_sched_factor'] = 0.5
+    hypers['lr_sched_patience'] = 5
     # ========================
     return hypers
 
